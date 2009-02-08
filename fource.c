159dfe594920657d71b03045d557dbd9f72032ae
Wojciech Meyer <specuu@gmail.com>
2009-02-08 02:43:55 +0000

    Tide up the code. Context switching. Lots of improvements

--- fource.c
+++ fource.c
@@ -1,11 +1,60 @@
 #include <stdio.h>
+#include <assert.h>
 
-char* vm_interpret(const char* buffer);
+
+
+char* Vm_interpret(const char* buffer);
+void  Vm_reset    (void);
+
+
+
+
+/* TODO: Make it printf friendly... */
+#define PANIC(_msg,rcode) ({ fprintf(stderr,"Fatal: %s\n", (_msg)); exit(rcode); })
+#define FEAR(_msg) ({ fprintf(stderr, "Warning: %s\n", (_msg)); })
+
+
+
+
+/* TODO: Make it windows friendly... */
+int just_one_line(FILE* f, int max_buffer, char* o_buffer)
+{
+  assert(o_buffer != NULL);
+  assert(f != NULL);
+  assert(max_buffer > 1);
+  
+  int nread=0, max_read = max_buffer-1, ch=0;
+  while( nread < max_read &&
+	 EOF != (ch = fgetc(f)) &&
+	 ch != '\n' )
+    { o_buffer[nread] = ch; ++nread; }
+
+  o_buffer[nread] = '\0';
+
+  if ( nread == max_read )
+    {
+      if ( ch != EOF )
+	{
+	  char msg[128];
+	  sprintf(msg, "Line exceeded %d characters. Truncated.", max_read-1);
+	  FEAR(msg);
+	  return -2;
+	}
+    }
+  
+  if ( ch == EOF )
+    return EOF;
+
+  return nread;
+}
+
+
+
 
 int main()
 {
-  char line[128];
-  while( NULL != gets(line) )
-    vm_interpret(line);
+  char line[257];
+  while( EOF != just_one_line(stdin, 256, line) )
+      Vm_interpret(line);
   return 0;
 }
