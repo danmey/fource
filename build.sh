04d8f074a7d00dd781ab4a5fe32499c0dde4b16a
Wojciech Meyer <specuu@gmail.com>
2009-02-12 22:41:21 +0000

    Some minor changes

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 
 #m4 fource.S > __main.S
 #gcc -Os -fomit-frame-pointer -o fource __main.S
-gcc -Os -fomit-frame-pointer -o fource engine.S fource.c
+gcc -g -Os -fomit-frame-pointer -o fource engine.S fource.c
 rm *.o
