468b33956661bbdf6b557123539822410066c20c
Wojciech Meyer <specuu@gmail.com>
2009-02-15 14:48:20 +0000

    clean up. put macros into separare GAS headers. remove unneeded stuff. still there is bug in getkey module that skips first word. continuations work well

--- build.sh
+++ build.sh
@@ -4,6 +4,4 @@
 #m4 fource.S > __main.S
 #gcc -Os -fomit-frame-pointer -o fource __main.S
 # gcc -g -Os -fomit-frame-pointer -o fource engine.S fource.c
-gcc  -c -ggdb3  -Os -fomit-frame-pointer -o fource.o  fource.c
-gcc  -c -g cont.S -o cont.o
-gcc -o fource cont.o fource.o
+gcc  -ggdb3 -Os -fomit-frame-pointer -o fource  fource.c getkey.S
