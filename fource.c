4f1bebda2196f96f45a549a2599a561e375a6795
Wojciech Meyer <specuu@gmail.com>
2009-02-18 21:59:52 +0000

    refactor code.

--- fource.c
+++ fource.c
@@ -18,49 +18,49 @@ void  Vm_reset    (void);
 /* VERY CLUMSY! */
 int just_one_line(FILE* f, int max_buffer, char* o_buffer)
 {
-  assert(o_buffer != NULL);
-  assert(f != NULL);
-  // Need space for "\n\0"
-  assert(max_buffer > 2);
+    assert(o_buffer != NULL);
+    assert(f != NULL);
+    // Need space for "\n\0"
+    assert(max_buffer > 2);
   
-  int nread=0, max_read = max_buffer-2, ch=0;
-  while( nread < max_read &&
-	 EOF != (ch = fgetc(f)) &&
-	 ch != '\n' )
-    { o_buffer[nread] = ch; ++nread; }
+    int nread=0, max_read = max_buffer-2, ch=0;
+    while( nread < max_read &&
+	   EOF != (ch = fgetc(f)) &&
+	   ch != '\n' )
+	{ o_buffer[nread] = ch; ++nread; }
 
-  if ( nread == max_read )
-    {
-      if ( ch != EOF )
+    if ( nread == max_read )
 	{
-	  char msg[128];
-	  FEAR("Line exceeded %d characters. Truncated.",msg);
-	  return -2;
+	    if ( ch != EOF )
+		{
+		    char msg[128];
+		    FEAR("Line exceeded %d characters. Truncated.",msg);
+		    return -2;
+		}
 	}
-    }
-
-  if ( ch == '\n')
-    o_buffer[nread++] = '\n';
     
-  o_buffer[nread++] = '\0';
+    if ( ch == '\n')
+	o_buffer[nread++] = '\n';
+    
+    o_buffer[nread++] = '\0';
 
-  if ( ch == EOF )
-    return EOF;
-
-  return nread;
+    if ( ch == EOF )
+	return EOF;
+  
+    return nread;
 }
 
 
 int main()
 {
-  char line[257];
-  while( EOF != just_one_line(stdin, 256, line) )
-    {
-      char* word = Vm_interpret(line);
-      //      puts("ala");
-      //      if ( word != NULL )
-	//	puts(word);
-      //
-    }
-  return 0;
+    char line[257];
+    while( EOF != just_one_line(stdin, 256, line) )
+	{
+	    char* word = Vm_interpret(line);
+	    //      puts("ala");
+	    //      if ( word != NULL )
+	    //	puts(word);
+	    //
+	}
+    return 0;
 }
