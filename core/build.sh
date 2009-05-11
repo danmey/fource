79e8a8a943c0bab8792a7c9fe38d4f1ae08d58ca
Wojciech Meyer <specuu@gmail.com>
2009-05-11 22:15:24 +0100

    corrected small error in build script

--- core/build.sh
+++ core/build.sh
@@ -4,6 +4,7 @@ export SFLAGS="-I../shared -c -ggdb3 -Wa,-g"
 #gcc -c -S vm_init.c
 #grep -v ${ignore_directives} vm_init.s > temp.s && mv temp.s vm_init.s
 gcc ${SFLAGS} main.S -o bin/main.o
+test -d bin || mkdir bin
 cd bin && ar rcs libfource-core.a main.o 
 cd ..
 
