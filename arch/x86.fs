cf06885241b44ae5e1f2f5825f2e870dd2904e4f
Wojciech Meyer <specuu@gmail.com>
2009-06-02 23:56:48 +0100

    basic assembly words. moving register to register and immediate to register. new forth words >r,r>,if,else,then. not case sensitive interpreter

--- arch/x86.fs
+++ arch/x86.fs
@@ -2,8 +2,8 @@
 0 Constant ax   1 Constant cx   2 Constant dx   3 Constant bx
 4 Constant sp   5 Constant bp   6 Constant si   7 Constant di
 8 Constant al   9 Constant cl  $a Constant dl  $b Constant bl
-\ $c Constant ah  $d Constant ch  $e Constant dh  $f Constant bh
-
+$c Constant ah  $d Constant ch  $e Constant dh  $f Constant bh
+\ ALU instructions
 0 Constant #add
 1 Constant #adc
 2 Constant #sbb
@@ -11,17 +11,12 @@
 4 Constant #sub
 5 Constant #xor
 6 Constant #cmp
+\ regsrc regdst modifier -- 
+: addrb, 3 And 6 Lshift swap 7 and Lshift 3 Or swap 7 And Or c, ;
+\ immediate value tag
+: # $ff ;
+: mov, swap dup $ff = if drop $b8 + c, , else swap $8b c, 3 addrb, then ; immediate
+: test [ 100 # bx mov, bx ax mov, ] ;
+123 test . .
 
-
-: $b,
-    3 6 shiftl >r
-    3 shiftl r> and
-    and
-;
-
-: reg,
-    
-    
-: movl,
-    $8a literal
     
