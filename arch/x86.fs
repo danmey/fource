aaa950df3d75ef398dbcfb044a0e40eb6b5b3476
Wojciech Meyer <specuu@gmail.com>
2009-06-05 20:19:37 +0100

    loop statements

--- arch/x86.fs
+++ arch/x86.fs
@@ -1,34 +1,39 @@
 \ 8086 registers
+: constant : literal postpone ; ;  immediate
+
+: [constant] postpone constant postpone immediate ; immediate
+
 0 Constant ax   1 Constant cx   2 Constant dx   3 Constant bx
 4 Constant sp   5 Constant bp   6 Constant si   7 Constant di
 8 Constant al   9 Constant cl  $a Constant dl  $b Constant bl
 $c Constant ah  $d Constant ch  $e Constant dh  $f Constant bh
 \ ALU instructions
-0 Constant #add
-1 Constant #adc
-2 Constant #sbb
-3 Constant #and
-4 Constant #sub
-5 Constant #xor
-6 Constant #cmp
+0 Constant add
+1 Constant adc
+2 Constant sbb
+3 Constant and
+4 Constant sub
+5 Constant xor
+6 Constant cmp
 \ Condition codes
 0 Constant eq 1 Constant ne
 \ regsrc regdst modifier -- 
 : addrb, 3 And 6 Lshift swap 7 and Lshift 3 Or swap 7 And Or c, ;
 \ immediate value tag
 : # $ff ;
-: inc, $40 + c, ; : dec, $48 + c, ;
-: jmp, $eb c, 2 - c, ; immediate 
-: mov, swap dup # = if drop $b8 + c, , else swap $8b c, 3 addrb, then ; immediate
+: inc $40 + c, ; : dec $48 + c, ;
+: jmp $eb c, 2 - c, ;
+: mov swap dup # = if drop $b8 + c, , else swap $8b c, 3 addrb, then ;
+
 : test
-    [
-    100 # bx mov,
-    bx ax mov,
-    inc,
-    dec,
-    dec,
+    100 # bx mov
+    bx ax mov
+    inc
+    dec
+    dec
     ] ;
 
-: test-jmp 1 [ here ] dup . [ ax inc, here - jmp, ] ;
+: test-jmp 1 [ here ] dup . ax inc [ here - ] jmp ;
+
 
 test-jmp
